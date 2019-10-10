3cb452a2f0d3ee0816fb58e9d587c166798ec272
nicodamusalois@gmail.com <52221462+nickosmas@users.noreply.github.com>
2019-10-10 03:00:08 -0500

    WebServer Part II update

--- WindowsWebServer/Form1.cs
+++ WindowsWebServer/Form1.cs
@@ -13,6 +13,28 @@ namespace WindowsWebServer
 {
     public partial class Form1 : Form
     {
+        // Threadsafe IsRunning flag to keep track of running state
+        private bool _isRunning;
+        static readonly object _isRunningLock = new object();  
+        public bool IsRunning
+        {
+            get
+            {
+                lock (_isRunningLock)
+                {
+                    return this._isRunning;
+                }
+            }
+            set
+            {
+                lock (_isRunningLock)
+                {
+                    this._isRunning = value;
+                }
+                button1.Enabled = !value;
+                button2.Enabled = value;
+            }
+        }
 
         public Form1()
         {
@@ -25,79 +47,70 @@ namespace WindowsWebServer
         //Set button2 click properties
         private void Button1_Click(object sender, EventArgs e)
         {
+            //Deactivate button1 and activate button2
             button1.Enabled = false;
             button2.Enabled = true;
 
-            ThreadPool.QueueUserWorkItem(SetBackGroundData, this);
+            IsRunning = true;
+            ThreadPool.QueueUserWorkItem(SetBackGroundData, this.IsRunning);
 
-        }
-
-        //Set button click properties
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            button1.Enabled = true;
-            button2.Enabled = false;
-        }
-
-        //Set Background Data
-        public void SetBackGroundData(Object value)
-        {
-            Thread.Sleep(5000);
-        }
-
-        private void StartTcpListener(object arg)
-        {
-            TcpListener listener;
-            int port = 5326;
-            bool IsRunning = (bool)arg;
-
-            listener = new TcpListener(IPAddress.Any, port);
-            listener.Start();
-            while (IsRunning)
+            // Start the main object's listener
+            main.listener = new TcpListener(IPAddress.Any, main.port);
+            main.listener.Start();
+            while (main.IsRunning) //Run until we're told to exit
             {
                 try
                 {
-                    TcpClient client = listener.AcceptTcpClient();
-
-                    ThreadPool.QueueUserWorkItem(GetRequestedItem, client);
+                    // Each new request to the listener get a TcpClient
+                    TcpClient lClient = main.listener.AcceptTcpClient();
+                    // Send that TcpClient to its own thread to process
+                    ThreadPool.QueueUserWorkItem(GetRequestedItem, lClient);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine("Error received: " + ex.Message);
                 }
             }
+
         }
 
-        private void GetWebData(object input)
+        //Set button click properties
+        private void Button2_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string strInput = input.ToString();
-                // Response Data initialization
-                Byte[] output = new Byte[1024];
-                string response = String.Empty;
-                // Create a new TCP Client
-                var tcpClient = new TcpClient(strInput, 80);
-                NetworkStream ns = tcpClient.GetStream();
-                Console.WriteLine("Connected");
+            //Deactivate button 2 and activate button1
+            button1.Enabled = true;
+            button2.Enabled = false;
 
-                // Write a request to the web server
-                Byte[] cmd = System.Text.Encoding.ASCII.GetBytes("GET / HTTP/1.0 \nHost: " + strInput + "\n\n");
-                ns.Write(cmd, 0, cmd.Length);
-                // Get the output
-                Int32 bytes = ns.Read(output, 0, output.Length);
-                while (bytes > 0)
-                {
-                    response += System.Text.Encoding.ASCII.GetString(output);
-                    bytes = ns.Read(output, 0, output.Length);
-                }
-                Thread.Sleep(5000);
-                SetForegroundData(response);
-            }
-            catch (Exception Ex)
-            {
-                SetForegroundData(Ex.Message);
-            }
+            IsRunning = false;
         }
+
+        //Set Background Data
+        public void StartListene(Object info)
+        {
+            if (info == null)
+                return;
+
+            fMain main = (fMain)info;
+            string response = String.Empty;
+            int nBytes = 0;
+
+            while (main.IsRunning)
+            {
+                try
+                {
+                    TcpClient client = main.listener.AcceptClient;
+                    ThreadPool.QueueUserWorkItem(GetUserItem, client);
+                }
+                catch (Exception ex){
+                    Console.WriteLine("Error received" + ex.Message);
+                }
+            }
+
+           // Thread.Sleep(5000);
+        }
+
+        
+
+       
     }
 }
